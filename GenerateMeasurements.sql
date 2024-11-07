8f826f670b4a8621f3d63e9d864a45e57fce699c
keithpohl82 <keith.pohl82@gmail.com>
2024-11-06 22:59:43 -0600

    Added sql script files to help seed database with ingredients and measurements.

---
+++ GenerateMeasurements.sql
@@ -0,0 +1,16 @@
+insert into measurements (id, abbreviation, measurement) value(1, "tbsp", "tablespoon");
+insert into measurements (id, abbreviation, measurement) value(2, "tsp", "teaspoon");
+insert into measurements (id, abbreviation, measurement) value(3, "oz", "ounce");
+insert into measurements (id, abbreviation, measurement) value(4, "fl. oz", "fluid ounce");
+insert into measurements (id, abbreviation, measurement) value(5, "c", "cup");
+insert into measurements (id, abbreviation, measurement) value(6, "qt", "quart");
+insert into measurements (id, abbreviation, measurement) value(7, "pt", "pint");
+insert into measurements (id, abbreviation, measurement) value(8, "gal", "gallon");
+insert into measurements (id, abbreviation, measurement) value(9, "lb", "pound");
+insert into measurements (id, abbreviation, measurement) value(10, "mL", "milliliter");
+insert into measurements (id, abbreviation, measurement) value(11, "g", "grams");
+insert into measurements (id, abbreviation, measurement) value(12, "kg", "kilogram");
+insert into measurements (id, abbreviation, measurement) value(13, "l", "liter");
+
+
+select * from measurements;
