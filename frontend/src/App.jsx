7072568d5fe0659e8720b03db96931cfaba7d62f
keithpohl82 <keith.pohl82@gmail.com>
2024-11-10 19:53:22 -0600

    Removed UserContainer as it was not needed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import { UserContainer } from './components/UserContainer';
+
 import AddIngredients from './components/AddIngredients';
 import AddRecipe from './components/AddRecipe';
 import IngredientsList from './components/IngredientsList';
 import SelectForm from './components/SelectForm';
 import Directions from './components/Directions';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => {
   return (
     <div className='container'>
-      <Login />
+      <Register />
     </div>
   );
 };
