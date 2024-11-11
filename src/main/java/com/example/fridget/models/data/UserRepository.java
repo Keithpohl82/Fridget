d6af7b228191587702503082a1eccb48d7d25ddb
keithpohl82 <keith.pohl82@gmail.com>
2024-11-10 20:31:13 -0600

    Added findByUserEmail to UserRepository for future use.

--- src/main/java/com/example/fridget/models/data/UserRepository.java
+++ src/main/java/com/example/fridget/models/data/UserRepository.java
@@ -3,10 +3,9 @@ package com.example.fridget.models.data;
 import com.example.fridget.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-
+    Optional<User> findByUserEmail(String emailAddress);
 }
