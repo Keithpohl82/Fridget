4f6c429ccad22367bdcb8f407e2dd1bc9b86f393
keithpohl82 <keith.pohl82@gmail.com>
2024-11-12 19:50:57 -0600

    Created Recipes page/component. Updated login redirect to recipes page.

---
+++ frontend/src/components/Recipes.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
+const Recipes = () => {
+
+    const [recipeList, setRecipes] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(
+              "http://localhost:8080/recipes/"
+            );
+            setRecipes(response.data);
+            
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+      
+      return (
+        <div>
+          {recipeList.map(recipe => (
+            <div key={recipe.id} className="recipe-container">
+                
+              <h2>Name:{recipe.name}</h2>
+              <h4>Description:{recipe.description}</h4>
+              <p>Prep Time: {recipe.prepTime}<br/> Cook Time: {recipe.cookTime}<br/> Total time: {recipe.totalTime}</p>
+              {recipeList.map(ingredient => (<div key={ingredient.id}>{ingredient.name}</div>))}
+              <p>Directions: {recipe.recipeDirections}</p>
+            </div>
+          ))}
+        </div>
+      );
+      
+};
+
+export default Recipes;
