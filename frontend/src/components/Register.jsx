157ab2be6133009848695c2fc51f1b445c8eeda5
Carddera1842 <carddera1842@gmail.com>
2024-11-14 20:26:15 -0600

    Replaced css using Bulma Library. Added Mock Home page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,43 +1,58 @@
 import { useState, useEffect } from "react";
-import styles from "../styles/Register.module.css";
+import clean from '../Pictures/Clean-Plate-Chef.jpg'; // Avatar Image
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [pwHash, setPassword] = useState("");
+  const [reenterPassword, setReenterPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [userEmail, setUserEmail] = useState("");
-  const [error, setError] = useState(""); // State for registration error messages
-  const [usernameMessage, setUsernameMessage] = useState(""); // State for username availability message
+  const [error, setError] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [passwordMatchMessage, setPasswordMatchMessage] = useState("");
 
   const checkUsernameAvailability = async (username) => {
     if (username.trim() === "") {
-      setUsernameMessage(""); // Clear message if input is empty
+      setUsernameMessage("");
       return;
     }
 
     try {
       const response = await fetch(`http://localhost:8080/api/users/check-username?username=${username}`);
       const message = await response.text();
-      setUsernameMessage(message); // Set message based on response
+      setUsernameMessage(message);
     } catch (error) {
       console.error("Error checking username:", error);
-      setUsernameMessage("Error checking username"); // Error message if request fails
+      setUsernameMessage("Error checking username");
     }
   };
 
-  // useEffect to call the check function when username changes
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       checkUsernameAvailability(username);
-    }, 500); // Delay of 500ms to reduce API calls while typing. Hitting endpoints too rapidly is bad. Might need to extend this delay for performance.
+    }, 500);
 
     return () => clearTimeout(delayDebounceFn);
   }, [username]);
 
+  // Check if the passwords match
+  useEffect(() => {
+    if (reenterPassword && pwHash !== reenterPassword) {
+      setPasswordMatchMessage("Passwords do not match");
+    } else {
+      setPasswordMatchMessage("");
+    }
+  }, [pwHash, reenterPassword]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
+
+    if (pwHash !== reenterPassword) {
+      setError("Passwords do not match. Please try again.");
+      return;
+    }
 
     const response = await fetch("http://localhost:8080/api/users/register", {
       method: "POST",
@@ -55,29 +70,153 @@ const Register = () => {
       alert(result);
       setUsername("");
       setPassword("");
+      setReenterPassword("");
       setFirstName("");
       setLastName("");
       setUserEmail("");
-      setUsernameMessage(""); // Clear username availability message
+      setUsernameMessage("");
+      setPasswordMatchMessage("");
     }
   };
 
   return (
-    <div className={styles.login} id={styles.rcorners2}>
-      <form onSubmit={handleSubmit}>
-        <h1>Register</h1>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <div className={styles.imgcontainer}>
-          <img src="https://img.freepik.com/free-vector/men-chef-holding-plate-cartoon-food-restaurant-logo-hand-draw-vector-illustration_56104-2135.jpg" alt="Avatar" className={styles.avatar} />
+    <div className="container">
+      <form onSubmit={handleSubmit} className="box">
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <h1 className="title has-text-centered">Register</h1>
+          </div>
         </div>
-        <div className={styles.container}>
-          <input className={styles.username} type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-          {usernameMessage && <p style={{ color: usernameMessage.includes("available") ? "green" : "red" }}>{usernameMessage}</p>}
-          <input className={styles.password} type="password" placeholder="Password" value={pwHash} onChange={(e) => setPassword(e.target.value)} required />
-          <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
-          <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-          <input type="email" placeholder="Email Address" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} required />
-          <button type="submit">Register</button>
+
+        {error && <p className="help is-danger">{error}</p>}
+
+        <div className="columns is-centered">
+          <div className="column is-one-quarter">
+            <div className="image is-256x256 is-rounded">
+              <img src={clean} alt="Avatar" />
+            </div>
+          </div>
+        </div>
+
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="field">
+              <div className="control has-icons-left has-icons-right">
+                <input
+                  className={`input ${usernameMessage.includes("available") ? "is-success" : "is-danger"}`}
+                  type="text"
+                  placeholder="Enter your username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-user"></i>
+                </span>
+                <p className={`help ${usernameMessage.includes("available") ? "is-success" : "is-danger"}`}>
+                  {usernameMessage}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Password and Re-enter Password fields */}
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="columns">
+              <div className="column is-half">
+                <div className="field">
+                  <div className="control has-icons-left">
+                    <input
+                      className="input"
+                      type="password"
+                      placeholder="Enter your password"
+                      value={pwHash}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-half">
+                <div className="field">
+                  <div className="control has-icons-left">
+                    <input
+                      className={`input ${passwordMatchMessage ? "is-danger" : "is-success"}`}
+                      type="password"
+                      placeholder="Re-enter your password"
+                      value={reenterPassword}
+                      onChange={(e) => setReenterPassword(e.target.value)}
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                    <p className="help is-danger">{passwordMatchMessage}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* First Name and Last Name inputs */}
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="field">
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter your first name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="field">
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter your last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="field">
+              <input
+                className="input"
+                type="email"
+                placeholder="Enter your email"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="field is-grouped is-grouped-centered">
+          <button type="submit" className="button is-link">
+            Register
+          </button>
+          <button type="reset" className="button is-light">
+            Cancel
+          </button>
         </div>
       </form>
     </div>
