09e063bf68cfab4ebd50cd7397c30f8176e27bdc
keithpohl82 <keith.pohl82@gmail.com>
2024-11-20 16:52:33 -0600

    removed measurements

--- src/main/java/com/example/fridget/models/data/MeasurementsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fridget.models.data;
-
-import com.example.fridget.models.Measurements;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MeasurementsRepository extends JpaRepository<Measurements, Long> {
-}
